0022e3a07f6f2f423376f31f3cc75bd1d287445f
Netanel Hadad <netanel.hadad3@gmail.com>
2025-06-14 21:11:20 +0300

    first commit

---
+++ graphics.py
@@ -0,0 +1,21 @@
+import graphs
+import pygame
+import math
+
+# represents a vertex as a circle on the screen
+class vertexObject:
+
+    # create new vertex object
+    def __init__(self, newColor, newPosition, newRadius, vertexInfo):
+        self.color = newColor
+        self.position = newPosition
+        self.radius = newRadius
+        self.vertexInfo = vertexInfo
+
+# represent an edge as an arrow on the screen
+class edgeObject:
+
+    # create new edge object
+    def __init__(self, edgeInfo, color):
+        self.edgeInfo = edgeInfo
+        self.color = color
